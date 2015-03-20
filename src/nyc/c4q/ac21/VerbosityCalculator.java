6025fd4532818e55f53726241746a0936e09a056
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-03-20 16:11:12 -0400

    Optimized some code and comments

--- src/nyc/c4q/ac21/VerbosityCalculator.java
+++ src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -12,13 +12,14 @@ public class VerbosityCalculator {
 
     public static void main(String[] args){
 
+        //declare and initializing variables
         double mobyDickWordCount = 0;
         double mobyDickSentenceCount = 0;
         double twoCitiesWordCount = 0;
         double twoCitiesSentenceCount = 0;
         double mobyDickVerbosity;
         double twoCitiesVerbosity;
-        DecimalFormat decimal = new DecimalFormat("#.##");
+        DecimalFormat decimal = new DecimalFormat("#.##"); //set a new DecimalFormat instance to display a maximum of two decimal places
 
         // An abstract representation of a File
         File mobyDick = new File("/Users/c4q-anthonyf/Desktop/accesscode/VerbosityCalculator/resources/MobyDick.txt");
@@ -32,19 +33,16 @@ public class VerbosityCalculator {
             // while loop continues while there is another word to search in the file
             while(scMobyDick.hasNext()) {
 
-                String tempWord = scMobyDick.next();// temporarily storing the current word into tempWord
+                String currentWord = scMobyDick.next();// temporarily storing the current word into currentWord
                 mobyDickWordCount++; // increments word counter
 
-                char lastChar = tempWord.charAt(tempWord.length()-1); // saving last char in the current word to a variable
+                char lastChar = currentWord.charAt(currentWord.length()-1); // saving last char in the current word to a variable
 
                 // nested if statements only counts a sentence if the word ends with a . ? or ! and
                 // the word is not Mr. Mrs. or Ms.
                 if(lastChar == '.' || lastChar == '?' || lastChar == '!'){
 
-
-                    if(tempWord.equals("Mr.") || tempWord.equals("Mrs.") || tempWord.equals("Ms.") ){
-                        // do not increment sentence Count
-                    }else {
+                    if( !(currentWord.equals("Mr.") || currentWord.equals("Mrs.") || currentWord.equals("Ms.") ) ){
                         mobyDickSentenceCount++;
                     }
                 }
@@ -56,25 +54,20 @@ public class VerbosityCalculator {
             // while loop continues while there is another word to search in the file
             while(scTwoCities.hasNext()) {
 
-                String tempWord = scTwoCities.next(); //temporarily storing the current word into tempWord
+                String currentWord = scTwoCities.next(); //temporarily storing the current word into currentWord
                 twoCitiesWordCount++; //increments word counter
 
-                char lastChar = tempWord.charAt(tempWord.length()-1); //saving last char in the current word to a variable
+                char lastChar = currentWord.charAt(currentWord.length()-1); //saving last char in the current word to a variable
 
                 // nested if statements only counts a sentence if the word ends with a . ? or ! and
                 // the word is not Mr. Mrs. or Ms.
                 if(lastChar == '.' || lastChar == '?' || lastChar == '!'){
 
-                    if(tempWord.equals("Mr.") || tempWord.equals("Mrs.") || tempWord.equals("Ms.") ){
-                        // do not increment sentence Count
-                    }else {
+                    if( !(currentWord.equals("Mr.") || currentWord.equals("Mrs.") || currentWord.equals("Ms.") ) ){
                         twoCitiesSentenceCount++;
                     }
                 }
             }
-
-
-
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
@@ -82,10 +75,16 @@ public class VerbosityCalculator {
 
         // Storing Verbosity for each file. Verbosity = (Total number of words)/(Total number of sentences)
         // I used Decimal Format to only show up to two decimal places
-        mobyDickVerbosity = Double.valueOf(decimal.format(mobyDickWordCount / mobyDickSentenceCount));
-        twoCitiesVerbosity = Double.valueOf(decimal.format(twoCitiesWordCount / twoCitiesSentenceCount));
+        mobyDickVerbosity = Double.valueOf(decimal.format( mobyDickWordCount / mobyDickSentenceCount ));
+        twoCitiesVerbosity = Double.valueOf(decimal.format( twoCitiesWordCount / twoCitiesSentenceCount ));
 
-        System.out.println("Hermal Melville's Moby Dick verbosity: " + mobyDickVerbosity);
+        //lists wordCount, sentenceCount and verbosity for each text
+        System.out.println("Moby Dick word count: " + mobyDickWordCount);
+        System.out.println("Moby Dick sentence count: " + mobyDickSentenceCount);
+        System.out.println("Hermal Melville's Moby Dick verbosity: " + mobyDickVerbosity + "\n");
+
+        System.out.println("A Tale of Two Cities word count: " + twoCitiesWordCount);
+        System.out.println("A Tale of Two Cities sentence count: " + twoCitiesSentenceCount);
         System.out.println("Charles Dickens' A Tale of Two Cities verbosity: " + twoCitiesVerbosity + "\n");
 
         // prints out which author is more verbose
